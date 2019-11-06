fb789ce6941b722e0ca4b054102e5626baa89fc9
Praful_G <prafulg@oscprofessionals.com>
2019-11-06 20:25:28 +0530

    Akshay: Files added

---
+++ json-test.js
@@ -0,0 +1,26 @@
+nodestudy = {
+	"data": "test",
+	init: function(data){
+		this.data = data;
+		console.log(this.data);
+	}
+} 
+
+nodestudy.init("abc");
+
+
+
+/*[{
+    "class": "1",
+    "studens": [{
+        "name": "Manish",
+        "score": "50"
+    },{
+        "name": "Tirupati",
+        "score": "50"
+    }],
+	"result": function(){
+		console.log("test");
+	}
+}];
+nodestudy[0].result();*/
